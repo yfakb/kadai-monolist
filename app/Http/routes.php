c8e5ff7220eb37d5085c6923680f3a8b5b12dbfa
yasukoyamashita <akubi0117@gmail.com>
2017-05-16 14:58:58 +0000

    user login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,5 +13,11 @@
 
 Route::get('/', 'WelcomeController@index');
 
+//登録画面
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
+
+//ログイン、ログアウト画面
+Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
+Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
+Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         
         Collective\Html\HtmlServiceProvider::class,
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
 
     ],
 
@@ -198,6 +199,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,        
 
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
     ],
 
 ];

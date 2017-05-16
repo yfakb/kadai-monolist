aed2eb124da33855ce137e473f2ed6cb3660c56c
yasukoyamashita <akubi0117@gmail.com>
2017-05-16 14:40:53 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,3 +12,6 @@
 */
 
 Route::get('/', 'WelcomeController@index');
+
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
